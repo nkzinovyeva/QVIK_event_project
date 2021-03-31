c46e3420977db854af896abd2466b37b0690151d
ColineFardel <coline.fardel@gmail.com>
2021-03-31 16:34:05 +0300

    Chevron and star color for passed events - Coline

--- components/listItem.js
+++ components/listItem.js
@@ -5,68 +5,85 @@ import theme from '../constants/theme';
 import AppFavButton from "../components/favButton"
 
 export default AppList = (props) => {
-    
-    const { 
-        leftIcon, 
+
+    const {
+        leftIcon,
         iconColor,
-        title, 
-        subtitle, 
-        rightTopSubtitle, 
-        rightBottomSubtitle, 
+        title,
+        subtitle,
+        rightTopSubtitle,
+        rightBottomSubtitle,
         status,
         item } = props;
 
-    const getStatus = (status) => {
-        if (status == "active") {
-            return 1
-        }
-        else if (status == "passed") {
-            return 2
-        }
-        else if (status == "canceled") {
-            return 3
-        }
-        else if (status == "closed") {
-            return 4
-        }
+    let chevronColor = theme.colors.grayColor;
+    let starColor = iconColor;
+    let statusNumber;
+
+    if (status == "active") {
+        chevronColor = theme.colors.blueColor;
+        statusNumber = 1;
     }
-        
+    else if (status == "passed") {
+        starColor = theme.colors.grayColor;
+        statusNumber = 2;
+    }
+    else if (status == "canceled") {
+        starColor = theme.colors.grayColor;
+        statusNumber = 3;
+    }
+    else if (status == "closed") {
+        chevronColor = theme.colors.redColor;
+        statusNumber = 4;
+    }
+
+    // const getStatus = (status) => {
+    //     if (status == "active") {
+    //         return 1
+    //     }
+    //     else if (status == "passed") {
+    //         return 2
+    //     }
+    //     else if (status == "canceled") {
+    //         return 3
+    //     }
+    //     else if (status == "closed") {
+    //         return 4
+    //     }
+    // }
+
     return (
         <ListItem bottomDivider >
-            {leftIcon && <AppFavButton item={item} color={iconColor} /> }
+            {leftIcon && <AppFavButton item={item} color={starColor} />}
             <ListItem.Content>
                 <ListItem.Title style={[
-                    (getStatus(status) == 1) ? styles.title : {},
-                    (getStatus(status) == 2) ? styles.passedTitle : styles.title,
-                    (getStatus(status) == 3) ? styles.canceledTitle : {},
-                    ]}>{title}</ListItem.Title>
+                    (statusNumber == 1) ? styles.title : {},
+                    (statusNumber == 2) ? styles.passedTitle : styles.title,
+                    (statusNumber == 3) ? styles.canceledTitle : {},
+                ]}>{title}</ListItem.Title>
                 <ListItem.Subtitle style={[
-                    (getStatus(status) == 1) ? styles.subtitle : {},
-                    (getStatus(status) == 2) ? styles.passedSubtitle : styles.subtitle,
-                    (getStatus(status) == 3) ? styles.canceledSubtitle : styles.subtitle,
-                    ]} >{status !== "canceled" ? subtitle : 'The event is canceled'}</ListItem.Subtitle>
+                    (statusNumber == 1) ? styles.subtitle : {},
+                    (statusNumber == 2) ? styles.passedSubtitle : styles.subtitle,
+                    (statusNumber == 3) ? styles.canceledSubtitle : styles.subtitle,
+                ]} >{status !== "canceled" ? subtitle : 'The event is canceled'}</ListItem.Subtitle>
             </ListItem.Content>
             <ListItem.Content style={styles.rightContent}>
                 <ListItem.Subtitle style={[
-                    (getStatus(status) == 1) ? styles.rightSubtitle : {},
-                    (getStatus(status) == 2) ? styles.passedRightSubtitle : styles.rightSubtitle,
-                    (getStatus(status) == 3) ? styles.canceledRightSubtitle : {},
-                    (getStatus(status) == 4) ? styles.closedRightSubtitle : {},
-                    ]}>{rightTopSubtitle}</ListItem.Subtitle>
+                    (statusNumber == 1) ? styles.rightSubtitle : {},
+                    (statusNumber == 2) ? styles.passedRightSubtitle : styles.rightSubtitle,
+                    (statusNumber == 3) ? styles.canceledRightSubtitle : {},
+                    (statusNumber == 4) ? styles.closedRightSubtitle : {},
+                ]}>{rightTopSubtitle}</ListItem.Subtitle>
                 <ListItem.Subtitle style={[
-                    (getStatus(status) == 1) ? styles.rightSubtitle : {},
-                    (getStatus(status) == 2) ? styles.passedRightSubtitle : styles.rightSubtitle,
-                    (getStatus(status) == 3) ? styles.canceledRightSubtitle : {},
-                    (getStatus(status) == 4) ? styles.closedRightSubtitle : {},
-                    ]}>{rightBottomSubtitle}</ListItem.Subtitle>
+                    (statusNumber == 1) ? styles.rightSubtitle : {},
+                    (statusNumber == 2) ? styles.passedRightSubtitle : styles.rightSubtitle,
+                    (statusNumber == 3) ? styles.canceledRightSubtitle : {},
+                    (statusNumber == 4) ? styles.closedRightSubtitle : {},
+                ]}>{rightBottomSubtitle}</ListItem.Subtitle>
             </ListItem.Content>
             <ListItem.Chevron
-                color={[
-                    (getStatus(status) == 1) ? theme.colors.blueColor : {},
-                    (getStatus(status) == 2) ? theme.colors.lightgrayColor : {},
-                    (getStatus(status) == 3) ? theme.colors.lightgrayColor : {},
-                    (getStatus(status) == 4) ? theme.colors.redColor : {},
-                    ]} />
+                color={chevronColor}
+            />
         </ListItem>
     )
 
@@ -75,15 +92,15 @@ export default AppList = (props) => {
 const styles = StyleSheet.create({
     title: {
         color: theme.colors.blackColor,
-        fontSize: theme.fontSizes.listTitle  
+        fontSize: theme.fontSizes.listTitle
     },
     passedTitle: {
         color: theme.colors.grayColor,
-        fontSize: theme.fontSizes.listTitle  
+        fontSize: theme.fontSizes.listTitle
     },
     canceledTitle: {
         color: theme.colors.grayColor,
-        fontSize: theme.fontSizes.listTitle  
+        fontSize: theme.fontSizes.listTitle
     },
     subtitle: {
         color: theme.colors.grayColor,
