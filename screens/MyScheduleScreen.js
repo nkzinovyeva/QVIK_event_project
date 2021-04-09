5fd91194f7806c3be609556eb34341568f34e7cc
ColineFardel <coline.fardel@gmail.com>
2021-04-09 17:07:31 +0300

    Fix my schedule sorting - Coline

--- screens/MyScheduleScreen.js
+++ screens/MyScheduleScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View,FlatList, TouchableOpacity, SafeAreaView, Text  } from 'react-native';
+import { StyleSheet, View, FlatList, TouchableOpacity, SafeAreaView, Text } from 'react-native';
 import moment from "moment";
 import { useSelector } from 'react-redux';
 import AppList from "../components/listItem";
@@ -9,28 +9,36 @@ import theme from '../constants/theme';
  * SCREEN FOR THE FAVOURITE EVENTS
 ****/
 
-export default function MyScheduleScreen({navigation}) {
+export default function MyScheduleScreen({ navigation }) {
 
   //constants
   const { setupData, favourites } = useSelector(state => state.eventsReducer);
 
   //sorting the list of favourites
-  favourites.sort((a, b) =>
-    a.startDate > b.startDate || a.startTime > b.startTime
-      ? 1
-      : -1
+  favourites.sort((a, b) => {
+    if (a.startDate === b.startDate) {
+      if (a.startTime > b.startTime)
+        return 1;
+      else return -1;
+    }
+    else {
+      if (a.startDate > b.startDate)
+        return 1;
+      else return -1;
+    }
+  }
   )
 
   //header component 
   React.useLayoutEffect(() => {
     navigation.setOptions({
-      header: () => 
-        <AppHeader 
+      header: () =>
+        <AppHeader
           img={setupData.eventImage}
           //img={require('../assets/mainPic.jpg')}
           //img={{uri: setupData.eventImage}}
-          title="My schedule" 
-          subTitle={setupData.title + ', ' + setupData.venue + ', ' + (moment(setupData.startDate).format("MMM Do") +  " - " + moment(setupData.endDate).format("Do YYYY"))}
+          title="My schedule"
+          subTitle={setupData.title + ', ' + setupData.venue + ', ' + (moment(setupData.startDate).format("MMM Do") + " - " + moment(setupData.endDate).format("Do YYYY"))}
           leftButton={false}
           rightButton={false}
           clickableTag={false}
@@ -39,34 +47,34 @@ export default function MyScheduleScreen({navigation}) {
   }, [navigation]);
 
   //render the event
-  const Event = ({item}) => {
+  const Event = ({ item }) => {
 
     //check the passed/future state of events
     let nowTime = moment().format('HH:mm:ss');
     let nowDate = moment().format('YYYY-MM-DD');
-    
+
     var status = "active";
-    if (item.active == false ) {
+    if (item.active == false) {
       status = "canceled";
     }
-    else if (item.startDate < nowDate || item.startDate == nowDate && item.startTime > nowTime  ) {
+    else if (item.startDate < nowDate || item.startDate == nowDate && item.startTime > nowTime) {
       status = "passed";
     }
 
     return (
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate("Event", item.eventId )}
-        >
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("Event", item.eventId)}
+      >
         <AppList
-            leftIcon={true}
-            iconColor={theme.colors.blackColor}
-            title={item.title}
-            subtitle={item.stage.name}
-            rightTopSubtitle={moment(item.startDate, "YYYY-MM-DD").format('ll')}
-            rightBottomSubtitle={moment(item.startTime, "HH:mm:ss").format('LT')}
-            status={status}
-            item={item}
+          leftIcon={true}
+          iconColor={theme.colors.blackColor}
+          title={item.title}
+          subtitle={item.stage.name}
+          rightTopSubtitle={moment(item.startDate, "YYYY-MM-DD").format('ll')}
+          rightBottomSubtitle={moment(item.startTime, "HH:mm:ss").format('LT')}
+          status={status}
+          item={item}
         />
       </TouchableOpacity>
     );
@@ -74,20 +82,20 @@ export default function MyScheduleScreen({navigation}) {
 
   //render the list of favourites
   return (
-      <SafeAreaView style={styles.container}>
-          {favourites.length === 0 ? (
-            <Text style={styles.replacementText}>
-              Add events to favourite list
-            </Text>
-          ) : (
-            <FlatList 
-                data={favourites}
-                keyExtractor={item => item.eventId.toString()} 
-                renderItem={({item}) => <Event item={item}/>}
-            />
-            )}
-      </SafeAreaView> 
-    );
+    <SafeAreaView style={styles.container}>
+      {favourites.length === 0 ? (
+        <Text style={styles.replacementText}>
+          Add events to favourite list
+        </Text>
+      ) : (
+        <FlatList
+          data={favourites}
+          keyExtractor={item => item.eventId.toString()}
+          renderItem={({ item }) => <Event item={item} />}
+        />
+      )}
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -97,9 +105,9 @@ const styles = StyleSheet.create({
     //paddingTop: StatusBar.currentHeight,
   },
   replacementText: {
-    color: theme.colors.grayColor, 
-    fontSize: 18, 
-    alignSelf: "center", 
+    color: theme.colors.grayColor,
+    fontSize: 18,
+    alignSelf: "center",
     marginTop: 50
   }
 }
